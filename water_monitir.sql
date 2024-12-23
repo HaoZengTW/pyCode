e41709ae58294547e8bfa6f2b78926be172c0979
peiyu <peggy.deng0314@gmail.com>
2024-12-23 15:22:03 +0800

    no message

--- water_monitir.sql
+++ water_monitir.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.38, for macos14 (x86_64)
 --
--- Host: localhost    Database: water monitor
+-- Host: localhost    Database: water_monitor
 -- ------------------------------------------------------
 -- Server version	9.0.1
 
@@ -16,12 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `log_table`
+--
+
+DROP TABLE IF EXISTS `log_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `log_table` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `station_name` varchar(45) DEFAULT NULL,
+  `notify_time` datetime DEFAULT NULL,
+  `status` tinyint DEFAULT NULL,
+  `rainfall_condition` float DEFAULT NULL,
+  `rainfall_10min` float DEFAULT NULL,
+  `error_msg` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `rainfall_web`
 --
 
+DROP TABLE IF EXISTS `rainfall_web`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE IF NOT EXISTS `rainfall_web` (
+CREATE TABLE `rainfall_web` (
   `primary_num` int NOT NULL AUTO_INCREMENT,
   `station_name` varchar(45) DEFAULT NULL,
   `region_name` varchar(45) DEFAULT NULL,
@@ -35,16 +55,17 @@ CREATE TABLE IF NOT EXISTS `rainfall_web` (
   `data_time` datetime DEFAULT NULL,
   `current_time` datetime DEFAULT NULL,
   PRIMARY KEY (`primary_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=15201 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20747 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `valve_table`
 --
 
+DROP TABLE IF EXISTS `valve_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE IF NOT EXISTS `valve_table` (
+CREATE TABLE `valve_table` (
   `valve_id` int NOT NULL AUTO_INCREMENT,
   `valve_close_time` datetime DEFAULT NULL,
   `before_size` int DEFAULT NULL,
@@ -53,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `valve_table` (
   `after_flow` int DEFAULT NULL,
   `current_time` datetime DEFAULT NULL,
   PRIMARY KEY (`valve_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -65,4 +86,4 @@ CREATE TABLE IF NOT EXISTS `valve_table` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-16 13:50:19
+-- Dump completed on 2024-12-19 14:21:07
